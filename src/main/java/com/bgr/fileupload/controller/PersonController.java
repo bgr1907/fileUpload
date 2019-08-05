c5ae5d88c0a911454af40e61e425aec9d7dc44c5
unknown <akarabacak38@gmail.com>
2019-08-05 15:01:53 +0300

    backend side editted(PersonController)

--- src/main/java/com/bgr/fileupload/controller/PersonController.java
+++ src/main/java/com/bgr/fileupload/controller/PersonController.java
@@ -4,12 +4,16 @@ import com.bgr.fileupload.domain.Response;
 import com.bgr.fileupload.model.Person;
 import com.bgr.fileupload.service.PersonService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.ServletContext;
+import java.io.File;
 import java.io.IOException;
 import java.util.Date;
 import java.util.List;
@@ -21,12 +25,44 @@ public class PersonController {
     PersonService personService;
 
     @PostMapping(value = "/saveUserProfile")
-    public ResponseEntity<Response> saveuserprofile(@RequestParam("file")MultipartFile file, @RequestParam("user") String user) throws IOException {
+    public ResponseEntity<Response> saveUserProfile(@org.jetbrains.annotations.NotNull @RequestParam("file")MultipartFile file, @RequestParam("user") String user) throws IOException {
 
         Person person = new ObjectMapper().readValue(user, Person.class);
         person.setFile(file.getBytes());
         person.setFileName(file.getOriginalFilename());
         person.setCreatedDate(new Date());
+        Person dbPerson = personService.save(person);
+        if(dbPerson!=null) {
+            return new ResponseEntity<Response>(new Response("User is saved successfully"), HttpStatus.OK);
+        }else {
+            return new ResponseEntity<Response>(new Response("User is not saved"), HttpStatus.BAD_REQUEST);
+        }
+
+    }
+
+    @PostMapping(value = "/saveUserProfileInServer")
+    public ResponseEntity<Response> saveUserProfileInServer(@RequestParam("file")MultipartFile file, @RequestParam("user") String user) throws IOException {
+
+        Person person = new ObjectMapper().readValue(user, Person.class);
+        person.setCreatedDate(new Date());
+
+        boolean isExist = new File("C:/Users/bgr/Desktop/file").exists();
+        if(!isExist){
+            new File("C:/Users/bgr/Desktop/file").mkdir();
+        }
+
+        String fileName = file.getOriginalFilename();
+        String modifiedFileName = FilenameUtils.getBaseName(fileName)+"_"+System.currentTimeMillis()+"."+FilenameUtils.getExtension(fileName);
+        File serverFile = new File("C:/Users/bgr/Desktop/file"+File.separator+modifiedFileName);
+
+        try{
+            FileUtils.writeByteArrayToFile(serverFile, file.getBytes());
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        person.setFileName(modifiedFileName);
+
         Person dbPerson = personService.save(person);
         if(dbPerson!=null) {
             return new ResponseEntity<Response>(new Response("User is saved successfully"), HttpStatus.OK);
